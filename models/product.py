3e8035c0b896710134ad231d651d30eb5c29d29e
Manuel Márquez <manuel@humanytek.com>
2017-05-01 14:21:49 -0400

    [ADD] product_price_visible_for_sale_purchase_users: Extends fields Sale Price and Cost of model product.template to make it visible only to users of sales and purchase.

--- models/product.py
+++ models/product.py
@@ -2,6 +2,16 @@
 
 from odoo import models, fields, api
 
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    list_price = fields.Float(
+        groups='purchase.group_purchase_user,sales_team.group_sale_salesman')
+    standard_price = fields.Float(
+        groups='purchase.group_purchase_user,sales_team.group_sale_salesman')
+
+
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
